48242d2d48e307bdf531bb079b6428e5de243f76
Muhammad Naufal Pahlevi <naufal260902@gmail.com>
2026-01-02 18:15:01 +0700

    initial commit

---
+++ dasar-javascript/control-flow-javascript/script.js
@@ -0,0 +1,12 @@
+for (var i = 1; i <= 5; i++) {
+  console.log("Iteration: " + i);
+}
+console.log("Loop finished.");
+
+var number = 3;
+if (number % 2 === 0) {
+  console.log(number + " adalah genap");
+} else {
+  console.log(number + " adalah ganjil");
+}
+console.log("End.");
---
+++ dasar-javascript/menulis-javascript/script.js
@@ -0,0 +1,5 @@
+var a = 10;
+var b = 5;
+var c = a + b;
+console.log("Hasil penjumlahan a + b = " + c);
+console.log("Apakah C lebih kecil dari 20 = " + (c < 20));
